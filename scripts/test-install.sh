1bb77e4043b5368c427e984ae2caf4a6943ba117
Jack Franklin <jacktfranklin@chromium.org>
2020-04-22 14:23:58 +0100

    chore: add test for npm package installing correctly
    This command packs up the module and installs it again to check we're
    correctly bundling everything we need to allow users to do a fresh
    install.

---
+++ scripts/test-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Pack the module into a tarball
+npm pack
+tarball=$(realpath puppeteer-*.tgz)
+cd "$(mktemp -d)"
+npm init --yes
+# Check we can install from the tarball.
+# This emulates installing from npm and ensures that:
+# 1. we publish the right files in the `files` list from package.json
+# 2. The install script works and correctly exits without errors
+# 3. Requiring Puppeteer from Node works.
+npm install "${tarball}" && node --eval="require('puppeteer')" && rm "${tarball}";
