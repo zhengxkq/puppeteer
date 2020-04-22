2a5908da8750d964729c80d2fd110531d2b1a86e
Jack Franklin <jacktfranklin@chromium.org>
2020-04-22 14:39:27 +0100

    silent npm loglevel

--- scripts/test-install.sh
+++ scripts/test-install.sh
@@ -5,12 +5,11 @@ set -e
 npm pack
 tarball="$(realpath puppeteer-*.tgz)"
 cd "$(mktemp -d)"
-npm init --yes
 # Check we can install from the tarball.
 # This emulates installing from npm and ensures that:
 # 1. we publish the right files in the `files` list from package.json
 # 2. The install script works and correctly exits without errors
 # 3. Requiring Puppeteer from Node works.
-npm install "${tarball}"
+npm install --loglevel silent "${tarball}"
 node --eval="require('puppeteer')"
 rm "${tarball}"
